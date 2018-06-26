28e6dd00664e73b3db90dd0c163b92ebd5c32b97
mocenas <mocenas@redhat.com>
2018-06-26 13:22:25 +0200

    Created openshift integration test

---
+++ tests/src/test/java/me/snowdrop/istio/dt/OpenshiftIT.java
@@ -0,0 +1,134 @@
+package me.snowdrop.istio.dt;
+
+
+import io.fabric8.openshift.api.model.v3_1.Route;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import me.snowdrop.istio.api.model.IstioResource;
+import org.arquillian.cube.istio.impl.IstioAssistant;
+import org.arquillian.cube.openshift.impl.client.OpenShiftAssistant;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@RunWith(Arquillian.class)
+public class OpenshiftIT {
+    @ArquillianResource
+    private IstioAssistant istioAssistant;
+
+    @ArquillianResource
+    private OpenShiftAssistant openShiftAssistant;
+
+    private static String istioURL;
+    private static String jaegerURL;
+    private static boolean isInit = false;
+    private static List<IstioResource> routeRule = null;
+
+    private final String appUrl = "greeting/";
+
+    @Before
+    public void init() throws Exception {
+        // initiate only before first test
+        if (!isInit) {
+            istioURL = "http://" + getIstioRoute("istio-ingress");
+            jaegerURL = "https://" + getIstioRoute("jaeger-query") + "api/traces";
+
+            routeRule = deployRouteRule("route-rule-redir.yml");
+            await().
+                    pollInterval(1, TimeUnit.SECONDS)
+                    .atMost(1, TimeUnit.MINUTES)
+                    .until(() -> {
+                try {
+                    Response response = RestAssured.get(istioURL + appUrl);
+                    return response.getStatusCode() == 200;
+                } catch (Exception ignored) {
+                    return false;
+                }
+            });
+
+            // jaeger is using HTTPS with self-signed certificate
+            // allow acceptance of non-trusty certificates
+            RestAssured.useRelaxedHTTPSValidation();
+
+            isInit = true;
+        }
+    }
+
+    @After
+    public void cleanup() {
+        if (routeRule != null){
+            istioAssistant.undeployIstioResources(routeRule);
+        }
+    }
+
+    /**
+     * Interacts with application and test if traces are present in jaeger
+     * @throws ParseException
+     * @throws InterruptedException
+     */
+    @Test
+    public void tracingTest() throws ParseException, InterruptedException {
+        Long startTime = TimeUnit.MILLISECONDS.toMicros(System.currentTimeMillis());
+
+        RestAssured
+                .get(istioURL + "greeting/api/greeting");
+
+        TimeUnit.SECONDS.sleep(10); // wait 10 seconds so span will show up in the jaeger
+
+        JSONArray jaegerTraces = getJaegerTraces(startTime);
+        Assert.assertEquals("Query should create one trace",1,jaegerTraces.size());
+        int spanCount = ((JSONArray)((JSONObject)jaegerTraces.get(0)).get("spans")).size();
+        Assert.assertTrue("Trace should have at least 4 spans, has: " + spanCount, spanCount >= 4);
+    }
+
+    private List<IstioResource> deployRouteRule(String routeRuleFile) throws IOException {
+        return istioAssistant.deployIstioResources(
+                Files.newInputStream(Paths.get("../rules/" + routeRuleFile)));
+    }
+
+    /**
+     * Get a route from istio namespace
+     * @param routeName name of the route to get
+     * @return route URL, without the protocol specification
+     * @throws Exception In route is not found
+     */
+    private String getIstioRoute(String routeName) throws Exception {
+        Route istioRoute = openShiftAssistant.getClient()
+                .routes()
+                .inNamespace("istio-system")
+                .withName(routeName)
+                .get();
+        if (istioRoute == null) {
+            throw new Exception("Istio " + routeName + " route not found");
+        }
+        return istioRoute.getSpec().getHost() + "/";
+    }
+
+    private JSONArray getJaegerTraces(Long startTime) throws ParseException {
+        Response response = RestAssured
+                .given()
+                    .param("service","istio-ingress")
+                    .param("start",startTime)
+                .get(jaegerURL);
+        JSONParser parser = new JSONParser();
+        JSONObject obj = (JSONObject)parser.parse(response.asString());
+
+        return (JSONArray)obj.get("data");
+    }
+}
