cea7094862b4cf7597d038ef90808f75be1e4e7d
Georgios Andrianakis <geoand@gmail.com>
2018-07-17 11:23:49 +0300

    Simplify integration test by using RouteURL.namespace

--- tests/src/test/java/me/snowdrop/istio/dt/OpenshiftIT.java
+++ tests/src/test/java/me/snowdrop/istio/dt/OpenshiftIT.java
@@ -3,18 +3,15 @@ package me.snowdrop.istio.dt;
 
 import static org.awaitility.Awaitility.await;
 
-import io.fabric8.openshift.api.model.v4_0.Route;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.arquillian.cube.istio.api.IstioResource;
-import org.arquillian.cube.openshift.impl.client.OpenShiftAssistant;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -23,23 +20,14 @@ import org.junit.runner.RunWith;
 public class OpenshiftIT {
 
     private static final String ISTIO_NAMESPACE = "istio-system";
-    private static final String ISTIO_INGRESS_GATEWAY_ROUTE = "istio-ingressgateway";
-    private static final String ISTIO_INGRESS_GATEWAY_SERVICE_NAME = "istio-ingressgateway";
-    private static final String JAEGER_QUERY_ROUTE = "jaeger-query";
+    private static final String JAEGER_QUERY_NAME = "jaeger-query";
+    private static final String ISTIO_INGRESS_GATEWAY_NAME = "istio-ingressgateway";
 
-    @ArquillianResource
-    private OpenShiftAssistant openShiftAssistant;
+    @RouteURL(value = JAEGER_QUERY_NAME, namespace = ISTIO_NAMESPACE)
+    private URL jaegerQueryURL;
 
-    private String istioIngressGatewayRouteURL;
-    private String jaegerTracesURL;
-
-    @Before
-    public void init() {
-        istioIngressGatewayRouteURL = "http://" + getRouteInIstioNamespace(ISTIO_INGRESS_GATEWAY_ROUTE);
-        jaegerTracesURL = "https://" + getRouteInIstioNamespace(JAEGER_QUERY_ROUTE) + "api/traces";
-
-        waitUntilApplicationIsReady();
-    }
+    @RouteURL(value = ISTIO_INGRESS_GATEWAY_NAME, namespace = ISTIO_NAMESPACE)
+    private URL ingressGatewayURL;
 
 
     /**
@@ -48,18 +36,24 @@ public class OpenshiftIT {
      */
     @Test
     public void tracingTest() throws InterruptedException {
+        waitUntilApplicationIsReady();
+
         Long startTime = TimeUnit.MILLISECONDS.toMicros(System.currentTimeMillis());
 
-        RestAssured.get(istioIngressGatewayRouteURL + "greeting/api/greeting");
+        RestAssured
+                .given()
+                .baseUri(ingressGatewayURL.toString())
+                .get("/greeting/api/greeting");
 
         TimeUnit.SECONDS.sleep(10); // wait 10 seconds so span will show up in the jaeger
 
         RestAssured
                 .given()
+                .baseUri(jaegerQueryURL.toString())
                 .relaxedHTTPSValidation() //jaeger uses https, so we need to trust the cert
-                .param("service", ISTIO_INGRESS_GATEWAY_SERVICE_NAME)
+                .param("service", ISTIO_INGRESS_GATEWAY_NAME)
                 .param("start",startTime)
-                .get(jaegerTracesURL)
+                .get("/api/traces")
                 .then()
                 .statusCode(200)
                 /*
@@ -111,26 +105,6 @@ public class OpenshiftIT {
                                  .count()) == 2;
                     }
                 });
-
-    }
-
-    /**
-     * Get a Route from istio namespace
-     * @param routeName name of the Route to get
-     * @return Route URL, without the protocol specification
-     */
-    private String getRouteInIstioNamespace(String routeName) {
-        Route istioRoute =
-                openShiftAssistant
-                        .getClient()
-                        .routes()
-                        .inNamespace(ISTIO_NAMESPACE)
-                        .withName(routeName)
-                        .get();
-        if (istioRoute == null) {
-            throw new RuntimeException(String.format("Expected Route %s in namespace %s was not found", routeName, ISTIO_NAMESPACE));
-        }
-        return istioRoute.getSpec().getHost() + "/";
     }
 
     // wait until the Istio Ingress Gateway responds with HTTP 200 at the path that is specified
@@ -141,8 +115,10 @@ public class OpenshiftIT {
             .atMost(1, TimeUnit.MINUTES)
                 .untilAsserted(() ->
                         RestAssured
+                                .given()
+                                .baseUri(ingressGatewayURL.toString())
                                 .when()
-                                .get(istioIngressGatewayRouteURL + "greeting/")
+                                .get("/greeting/")
                                 .then()
                                 .statusCode(200)
                 );
