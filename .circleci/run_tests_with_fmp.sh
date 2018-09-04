6072d5b6f1d34c23dbdb3e071bee3b3364542f2a
Georgios Andrianakis <geoand@gmail.com>
2018-09-04 17:39:10 +0300

    Update Istio installation to latest Maistra

--- .circleci/run_tests_with_fmp.sh
+++ .circleci/run_tests_with_fmp.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPT_ABSOLUTE_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 PROJECT_ABSOLUTE_DIR=$(dirname ${SCRIPT_ABSOLUTE_DIR})
 
 pushd ${PROJECT_ABSOLUTE_DIR} > /dev/null
 
-./mvnw clean verify -Popenshift,openshift-it "$@"
+MAVEN_OPTS="-Xmx256m" ./mvnw clean verify -Popenshift,openshift-it "$@"
 
 popd > /dev/null
